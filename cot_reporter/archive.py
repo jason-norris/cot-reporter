04ed87153185f004258a1a48a4954469ade8ca81
jason-norris <jason.w.norris@gmail.com>
2022-09-16 06:26:43 -0500

    Cleaning up comments

--- cot_reporter/archive.py
+++ cot_reporter/archive.py
@@ -10,8 +10,7 @@ wkDTS = datetime.now().strftime("%V")
 # Archiving week-stamped files, moving, and deleting from stage
 for file in stagelist:
     rename = f"{file.stem}_{wkDTS}"
-    # Open archive in "w" mode and write file to directory to not zip folder structure
-    zipfile.ZipFile(f"{arcpath}/{rename}.zip", mode='w', compression=zipfile.ZIP_DEFLATED).write(file, arcname=rename)
-    # Move file by replacing to archive
-    file_to_rem = Path(file) # Delete file after writing to master data file
+    # Opening archive in "w" mode and writing file to directory to not zip folder structure
+    zipfile.ZipFile(f"{arcpath}/{rename}.zip", mode='w', compression=zipfile.ZIP_DEFLATED).write(file, arcname=rename) # Not adding .txt
+    file_to_rem = Path(file) # Removing after writing to master data file
     file_to_rem.unlink()
