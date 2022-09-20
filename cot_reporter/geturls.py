ad67025a270038a68e8b232eec1d997970af3d97
jason-norris <jason.w.norris@gmail.com>
2022-09-20 17:01:47 -0500

    Rewriting as func and to accept install dir

--- cot_reporter/geturls.py
+++ cot_reporter/geturls.py
@@ -1,42 +1,52 @@
+#!/usr/bin/env python3
 import re
 import urllib.request
 from bs4 import BeautifulSoup
 from datetime import date
+from main import install_dir
 from pathlib import Path
 
-# Assigning opener agent to not look like a bot
-opener = urllib.request.build_opener()
-opener.addheaders = [("User-Agent", "Mozilla/5.0")]
-urllib.request.install_opener(opener)
+def get_report_links():
 
-# Retrieving website html
-urllib.request.urlretrieve("http://www.cftc.gov/MarketReports/CommitmentsofTraders/HistoricalCompressed/index.htm", "data/index.html")
+    cwd = Path(install_dir / "cot-reporter")
+    data_dir = Path(cwd / "data")
+    htmlfile = f"{data_dir}/index.html"
+    urlfile = f"{data_dir}/urls.txt"
 
-# Parsing html
-with open("data/index.html") as fp:
-    soup = BeautifulSoup(fp, "html.parser")
+    # Assigning opener agent to not look like a bot
+    opener = urllib.request.build_opener()
+    opener.addheaders = [("User-Agent", "Mozilla/5.0")]
+    urllib.request.install_opener(opener)
 
-# Defining regex function for desired reports
-def targetlink(href):
-    return href and re.compile(r"(fut_disagg_txt|fut_fin_txt)").search(href)
+    # Retrieving website html
+    urllib.request.urlretrieve("http://www.cftc.gov/MarketReports/CommitmentsofTraders/HistoricalCompressed/index.htm", htmlfile)
 
-# Required to conduct full historic load and then incremental loads week-over-week
-# 1) if nothing in archived location, grab all urls;
-# 2) if file in archive, then only url with current year
+    # Parsing html
+    with open(htmlfile) as fp:
+        soup = BeautifulSoup(fp, "html.parser")
 
-arcpath = Path("data/archive/")
-arclist = list(arcpath.glob("*.zip"))
-current_year = str(date.today().year)
+    # Defining regex function for desired reports
+    def targetlink(href):
+        return href and re.compile(r"(fut_disagg_txt|fut_fin_txt)").search(href)
 
-if len(arclist) == 0:
-    # Passing in a function to filter and return links
-    # Writing links for non-historical files
-    with open("data/urls.txt", "w") as file:
-        for link in soup.find_all(href=targetlink):
-            if "_hist_" not in str(link): # For fut_disagg_text, do not get 2006 - 2016 file
-                file.write("https://www.cftc.gov" + str(link.get("href") + "\n"))
-else:
-    with open("data/urls.txt", "w") as file:
-        for link in soup.find_all(href=targetlink):
-            if "_hist_" not in str(link) and str(link).rsplit("_txt_", 1)[1].rsplit(".", 1)[0] == current_year:
-                file.write("https://www.cftc.gov" + str(link.get("href") + "\n"))
+    # Required to conduct full historic load and then incremental loads week-over-week
+    # If nothing in archived location, grab all urls; else...
+    # if file in archive, then only url with current year
+
+    arcpath = Path(data_dir / "archive")
+    arclist = list(arcpath.glob("*.zip"))
+    current_year = str(date.today().year)
+
+    if len(arclist) == 0:
+        # Passing in a function to filter and return links
+        # Writing links for historical files (initial download)
+        with open(urlfile, "w") as file:
+            for link in soup.find_all(href=targetlink):
+                if "_hist_" not in str(link): # For fut_disagg_text, do not get 2006 - 2016 file
+                    file.write("https://www.cftc.gov" + str(link.get("href") + "\n"))
+    else:
+        # Writing links for non-historical files (weekly refresh)
+        with open(urlfile, "w") as file:
+            for link in soup.find_all(href=targetlink):
+                if "_hist_" not in str(link) and str(link).rsplit("_txt_", 1)[1].rsplit(".", 1)[0] == current_year:
+                    file.write("https://www.cftc.gov" + str(link.get("href") + "\n"))
