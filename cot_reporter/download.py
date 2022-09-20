8b20ec1e1e180be7a0623fe13390e088472ee688
jason-norris <jason.w.norris@gmail.com>
2022-09-20 17:00:57 -0500

    Rewriting as func, take install dir, and use plib

--- cot_reporter/download.py
+++ cot_reporter/download.py
@@ -1,30 +1,37 @@
-import os
+#!/usr/bin/env python3
 import urllib.request
 from datetime import date
+from main import install_dir
+from pathlib import Path
 
-# Assigning opener agent to help us not look like a bot
-opener = urllib.request.build_opener()
-opener.addheaders = [("User-Agent", "Mozilla/5.0")]
-urllib.request.install_opener(opener)
+def download_reports():
+    
+    cwd = Path(install_dir / "cot-reporter")
 
-# Assigning directory to check before downloading
-downloads_dir = os.path.abspath("data/downloaded")
+    # Assigning opener agent to help us not look like a bot
+    opener = urllib.request.build_opener()
+    opener.addheaders = [("User-Agent", "Mozilla/5.0")]
+    urllib.request.install_opener(opener)
 
-# For the list of urls...
-for url in open("data/urls.txt"):
+    # Assigning directory to check before downloading
+    downloads_dir = Path(cwd / "data/downloaded")
 
-    # Parsing source file names from list
-    # Removing new line characters
-    src_file = url.rsplit("/", 1)[-1].replace('\n','')
+    # For the list of urls...
+    data_dir = Path(cwd / "data")
+    urlfile = f"{data_dir}/urls.txt"
+    for url in open(urlfile):
 
-    # Creating current year string for source file comparison
-    current_year = str(date.today().year)
+        # Parsing source file names from list
+        # Removing new line characters
+        src_file = url.rsplit("/", 1)[-1].replace('\n','')
 
-    # Combining the name and the downloads directory to create destination path
-    dst_path = os.path.join(downloads_dir, src_file)
+        # Creating current year string for source file comparison
+        current_year = str(date.today().year)
 
-    # Downloading the file if it does not exist or if it is from the current year
-    # Splitting source file date stamp and comparing to current year
-    # !! Consider removing this line with changes made to geturls.py if/else for urls
-    if not os.path.isfile(dst_path) or src_file.rsplit("_", 1)[1].rsplit(".", 1)[0] == current_year:
-        urllib.request.urlretrieve(url, dst_path)
+        # Combining the name and the downloads directory to create destination path
+        dst_path = Path(downloads_dir / src_file)
+
+        # Downloading the file if it does not exist or if it is from the current year
+        # Splitting source file date stamp and comparing to current year
+        if not Path.is_file(dst_path) or src_file.rsplit("_", 1)[1].rsplit(".", 1)[0] == current_year:
+            urllib.request.urlretrieve(url, dst_path)
