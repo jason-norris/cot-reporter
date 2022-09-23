4e260f7071eb73bc78faf6535dbedb4346b37daf
jason-norris <jason.w.norris@gmail.com>
2022-09-23 07:37:57 -0500

    Moving imports to resolve circular dependencies

--- cot_reporter/main.py
+++ cot_reporter/main.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
-import geturls
-import download
-import extract
-import writemaster
-import archive
 from pathlib import Path
 from configparser import ConfigParser
 
 config = ConfigParser() 
 configFilePath = Path(__file__).parent / "config.ini"
 config.read(f"{configFilePath}")
-install_dir = Path(config.get("SETUP", "install_dir")) # Converting path string to object
+install_dir = Path(config.get("SETUP", "install_dir"))
 
 def main():
 
+    # Moving import inside function to resolve circular import
+    import geturls
     geturls.get_report_links()
+
+    import download
     download.download_reports()
+
+    import extract
     extract.extract_files()
+    
+    import writemaster
     writemaster.write_master_data()
+    
+    import archive
     archive.archive_files()
 
     print("Process complete!")
